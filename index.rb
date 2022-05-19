5d6af6e4dfec594debf95755f78b502897be47de
SpaceKun <you@example.com>
2022-05-19 12:55:45 +0300

    statistical

--- index.rb
+++ index.rb
@@ -13,7 +13,7 @@ library.save!
 # puts library.readers
 # puts library.books
 # puts library.orders.first.reader.name
-#  puts library.top_book(1)
+ puts library.top_book(1)
 # puts library.top_book(2).map { |book| book.title}
 # puts library.top_reader(2).map { |reader| reader.name}
 
